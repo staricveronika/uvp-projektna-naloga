7ac89e542c269d540bad577224a4a58d15b5a16b
staricveronika <staric.veronika@gmail.com>
2025-08-29 14:45:39 +0200

    Olepšanje kode.

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -20,8 +20,8 @@ HEADERS = {"User-agent": "Chrome/136.0.7103.114"}
 # ZBIRANJE PODATKOV
 
 def shrani_osnovne_htmlje(ZACETNA_STRAN, KONCNA_STRAN):
-    """Funkcija shrani HTML strani v mapo."""
-    os.makedirs(IMENIK_HTML, exist_ok=True) # ustvari mapo goodreads_html, če še ne obstaja, ne bo napake, če mapa že obstaja
+    """Funkcija shrani HTML strani seznama v mapo."""
+    os.makedirs(IMENIK_HTML, exist_ok=True) # ne bo napake, če mapa že obstaja
 
     for stran in range(ZACETNA_STRAN, KONCNA_STRAN + 1):
         url = f"{OSNOVNI_URL}?page={stran}" # sestavi url spletne strani, ki jo želimo pobrati
@@ -31,14 +31,12 @@ def shrani_osnovne_htmlje(ZACETNA_STRAN, KONCNA_STRAN):
         if html.status_code == 200: # če je zahteva uspešna
             ime_datoteke = os.path.join(IMENIK_HTML, f"stran_{stran}.html")
             with open(ime_datoteke, "w", encoding="utf-8") as f:
-                f.write(html.text) # html.text je vsebina HTML strani kot besedilo
+                f.write(html.text) # vsebina HTML strani kot besedilo
             print(f"Stran {stran} shranjena v {ime_datoteke}") # za preverjanje
         else:
             print(f"Napaka pri strani {stran}.")
 
         time.sleep(1) # počaka 1 sekundo, preden pošlje naslednjo zahtevo, da ne obremeni strežnika
-
-# shrani_osnovne_htmlje(ZACETNA_STRAN, KONCNA_STRAN)
 
 def pridobi_povezave_do_knjig():
     """Funkcija pridobi vse povezave do posameznih knjig znotraj HTML datotek in jih vrne v seznamu."""
@@ -49,22 +47,20 @@ def pridobi_povezave_do_knjig():
             with open(pot, encoding="utf-8") as f:
                 vsebina = f.read()
                 najdeno = re.finditer(r'href="/book/show/(\d+)[^"]*"', vsebina) # poišče vse linke, ki kažejo na posamezne knjige
-                # \d+ pomeni vsaj eno števko
-                # [^"] pomeni vsi znaki, ki niso narekovaj - tako dobimo vse do konca atributa
                 for link in najdeno:
-                    # sestavimo celoten url
-                    povezave.add("https://www.goodreads.com/book/show/" + link.group(1)) # dodamo celoten url v množico, group(1) predstavlja id knjige
-                    # https://www.goodreads.com/book/show/ID  
-    return list(povezave) # vrne seznam urljev do knjig
+                    povezave.add("https://www.goodreads.com/book/show/" + link.group(1)) 
+                    # sestavimo celoten url  
+    return list(povezave) # vrne seznam url-jev do knjig
 
 povezave = pridobi_povezave_do_knjig()
 
 def shrani_strani_posameznih_knjig(povezave):
     """Funkcija za vsako povezavo do knjige prenese HTML in ga shrani v mapo."""
-    os.makedirs(IMENIK_KNJIG_HTML, exist_ok=True) # ustvari mapo, če še ne obstaja
+    os.makedirs(IMENIK_KNJIG_HTML, exist_ok=True)
     for povezava in povezave:
-        ime_datoteke = povezava.split("/")[-1] + ".html" # ime datoteke naj bo zadnji del url-ja
-        # razdelimo url po poševnicah in vzamemo zadnji del, ki je ID knjige
+        ime_datoteke = povezava.split("/")[-1] + ".html" 
+        # ime datoteke naj bo zadnji del url-ja
+        # razdelimo url po poševnicah in vzamemo zadnji del (ID knjige)
         # dodamo .html, da bo datoteka shranjena kot HTML
         pot = os.path.join(IMENIK_KNJIG_HTML, ime_datoteke) # sestavimo pot do datoteke skupaj z imenom datoteke
         if not os.path.exists(pot): # če pot do datoteke še ne obstaja
@@ -74,26 +70,23 @@ def shrani_strani_posameznih_knjig(povezave):
                     f.write(odgovor.text) # vsebino html strani zapišemo v datoteko
                 print(f"{ime_datoteke} je shranjena.")
             else:
-                print(f"Napaka pri {povezava}.") # če status kode ni 200, izpišemo opozorilo, katera stran ni bila prenesena
+                print(f"Napaka pri {povezava}.")
             time.sleep(1)
         else:
             print(f"{ime_datoteke} že obstaja.") # če pot do datoteke že obstaja, jo preskočimo - ne prenašamo znova
 
-# shrani_strani_posameznih_knjig(povezave)
-
 # IZLUŠČIMO PODATKE
+
 def pocisti_besedilo(besedilo):
-    """Odstrani odvečne presledke in HTML entitete, ki se pojavijo v HTML."""
+    """Odstrani odvečne presledke in posebne znake, ki se pojavijo v HTML."""
     if besedilo:
-        besedilo = html.unescape(besedilo)    # &amp; spremeni v &
+        besedilo = html.unescape(besedilo) # &amp; spremeni v &
         besedilo = " ".join(besedilo.split()) # odstrani večkratne presledke
         return besedilo
-    return None
+    return None # če je besedilo None ali prazen niz
 
 def izlusci_osnovne_podatke(od, do):
     """Funkcija izlušči osnovne podatke o knjigah iz HTML strani seznama."""
-    # ne odpira posameznih strani knjig, npr. https://www.goodreads.com/book/show/12345
-    # namesto tega odpira HTML-je, ki smo jih shranili s seznama knjig, npr. goodreads_html/stran_1.html, stran_2.html
     osnovni_podatki = []
     vzorec = re.compile(
     r'<tr itemscope itemtype="http://schema.org/Book">.*?'
@@ -105,23 +98,23 @@ def izlusci_osnovne_podatke(od, do):
     )
 
     for stran in range(od, do + 1):
-        pot = os.path.join(IMENIK_HTML, f"stran_{stran}.html") # sestavi pot do ustrezne datoteke, npr. goodreads_html/stran_1.html
+        pot = os.path.join(IMENIK_HTML, f"stran_{stran}.html")
         with open(pot, encoding="utf-8") as f:
             vsebina = f.read()
             for najdba in vzorec.finditer(vsebina):
-                id_knjige = int(najdba["id_knjige"])
-                naslov = pocisti_besedilo(najdba["naslov"]) # najdba["ime_knjige"] vzame zajeti naslov knjige
+                id_knjige = int(najdba["id_knjige"]) 
+                naslov = pocisti_besedilo(najdba["naslov"])
                 avtor = pocisti_besedilo(najdba["avtor"])
-                povp_ocena = float(najdba["povp_ocena"]) # oceno pretvori v decimalno število
-                st_ocen = int(najdba["st_ocen"].replace(",", ""))
+                povp_ocena = float(najdba["povp_ocena"]) # oceno pretvorimo v decimalno število
+                st_ocen = int(najdba["st_ocen"].replace(",", "")) # število ocen pretvorimo v celo število
                 osnovni_podatki.append((id_knjige, naslov, avtor, povp_ocena, st_ocen))
     return osnovni_podatki
 
 def izlusci_podrobne_podatke_iz_knjige(osnovni_podatki):
     podrobni_podatki = []
 
-    for knjiga in osnovni_podatki: # osnovni podatki je seznam tuplov
-        id_knjige = knjiga[0]
+    for knjiga in osnovni_podatki: 
+        id_knjige = knjiga[0] # osnovni_podatki je seznam tuplov
         naslov = knjiga[1]
         avtor = knjiga[2]
         povp_ocena = knjiga[3]
@@ -136,8 +129,8 @@ def izlusci_podrobne_podatke_iz_knjige(osnovni_podatki):
         st_strani = None
         jezik = None
         leto_izdaje = None
-        st_trenutnih_bralcev = 0
         zanri_niz = None
+        st_trenutnih_bralcev = 0
         st_recenzij = 0
         
         # Število strani
@@ -151,18 +144,17 @@ def izlusci_podrobne_podatke_iz_knjige(osnovni_podatki):
         # Leto izdaje
         # podatek o tem je zapisan v JSON skripti na HTML strani
         import datetime
-        vzorec = re.search(r'"publicationTime":\s*(\d+)', vsebina)  # poišče v HTML vrstico kot je "publicationTime":1234567800000
+        vzorec = re.search(r'"publicationTime":\s*(\d+)', vsebina)
         # publicationTime je zapisan v UTC (Coordinated Universal Time)
         if vzorec:
-            timestamp_v_sekundah = int(vzorec.group(1)) / 1000 # pretvorimo timestamp iz milisekund v sekunde (delimo s 1000)
-            datum_v_utc = datetime.datetime.utcfromtimestamp(timestamp_v_sekundah) # iz timestampa naredimo datum (v UTC času)
+            timestamp_v_sekundah = int(vzorec.group(1)) / 1000 # pretvorimo iz milisekund v sekunde (delimo s 1000)
+            datum_v_utc = datetime.datetime.utcfromtimestamp(timestamp_v_sekundah) # naredimo datum (v UTC času)
             # funkcija utcfromtimestamp pretvori timestamp pretvori v normalen datum
-            # rezultat je recimo 2004-09-14 00:00:00
-            leto_izdaje = datum_v_utc.year # iz objekta datetime vrnemo samo leto --> 2004
+            # rezultat je recimo 2004-09-11 00:00:00
+            leto_izdaje = datum_v_utc.year # vrnemo samo leto --> torej 2004
         else:
-            leto_izdaje = None # če timestamp ni najden, vrnemo None
+            leto_izdaje = None # če timestamp ni najden
     
-
         # Trenutni bralci
         trenutni_bralci_re = re.search(r'([\d,]+)\s+people\s+are\s+currently\s+reading', vsebina)
         st_trenutnih_bralcev = int(trenutni_bralci_re.group(1).replace(",", "")) if trenutni_bralci_re else 0
@@ -170,7 +162,6 @@ def izlusci_podrobne_podatke_iz_knjige(osnovni_podatki):
         # Žanri
         zanri_re = re.findall(r'"bookGenres":\s*\[.*?\]', vsebina, re.DOTALL)
         if zanri_re:
-            # poiščemo vse "name":"..." znotraj tega bloka
             imena_zanrov = re.findall(r'"name":"(.*?)"', zanri_re[0])
             zanri_niz = ", ".join(imena_zanrov)
         else:
